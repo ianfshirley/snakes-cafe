e4c61a566ac5be8a4d16331a8b920fc8bca5db76
ianfshirley <ianfshirley88@gmail.com>
2022-12-19 17:37:39 -0800

    customer order function works properly

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -31,23 +31,48 @@ Drinks
 Coffee
 Tea
 Unicorn Tears
-
-***********************************
-** What would you like to order? **
-***********************************
 """
 
 
 print(intro)
 
 
+
 def customer_order():
     """
     Asks for and prints the item a user orders
     """
-    order = input("> ")
-    print(f"** 1 order of {order} has been added to your meal. **")
+    # print("** What would you like to order? **")
+    # order = input("> ").lower()
+    # print(f"** 1 order of {order} has been added to your meal. **")
+    menu = {
+        "wings": 0,
+        "cookies": 0,
+        "spring rolls": 0,
+        "salmon": 0,
+        "steak": 0,
+        "meat tornado": 0,
+        "a literal garden": 0,
+        "ice cream": 0,
+        "cake": 0,
+        "pie": 0,
+        "coffee": 0,
+        "tea": 0,
+        "unicorn tears": 0
+    }
+    ask_again = True
+    while ask_again:
+        print("** What would you like to order? **")
+        order = input("> ").lower()
+        if order == "quit":
+            ask_again = False
+            break
+        if order in menu.keys():
+            menu[order] += 1
+            print(f"** {menu[order]} orders of {order} have been added to your meal. **")
+        else:
+            print("Sorry, that item is unavailable. Please select an item from the menu.")
 
 
-if __name__ == "__snakes_cafe__":
+if __name__ == "__main__":
     customer_order()
